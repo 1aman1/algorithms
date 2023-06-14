03f91d9aa0f8f08992750c7533ccbeec0ffff77a
1aman1 <amangupta@outlook.com>
2023-06-14 16:27:31 +0530

    iterative

--- LEETCODE/704.Binary-Search.cpp
+++ LEETCODE/704.Binary-Search.cpp
@@ -1,29 +1,28 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution
 {
 public:
-    int B_search(int left, int right, vector<int> &nums, int target)
-    {
-        if (left <= right)
-        {
-            int mid = (right - left) / 2 + left;
-
-            if (nums[mid] == target)
-                return mid;
-
-            if (nums[mid] < target)
-                return B_search(mid + 1, right, nums, target);
-
-            return B_search(left, mid - 1, nums, target);
-        }
-        return -1;
-    }
-
     int search(vector<int> &nums, int target)
     {
-        return B_search(0, nums.size() - 1, nums, target);
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return -1;
     }
 };
