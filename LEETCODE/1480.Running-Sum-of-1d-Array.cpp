9e33e27012576eb50b95f64071dd7459d7943bb0
1aman1 <amangupta@outlook.com>
2023-06-15 19:23:04 +0530

    improved

--- LEETCODE/1480.Running-Sum-of-1d-Array.cpp
+++ LEETCODE/1480.Running-Sum-of-1d-Array.cpp
@@ -1,29 +1,15 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-// Input: nums = [1,2,3,4]
-// Output: [1,3,6,10]
-
 class Solution
 {
 public:
     vector<int> runningSum(vector<int> &nums)
     {
-        /*
-        for each element
-        STEP 2 read it's value
-        store it
-        and as we move ahead, we keep adding it to the value at current position, and STEP 2 again.
-
-        */
-
-        int store = 0;
+        int runningSum = 0;
         for (auto &i : nums)
         {
-            store += i;
-            i = store;
+            runningSum += i;
+            i = runningSum;
         }
+        
         return nums;
     }
 };
